40ba9e46fde18fbe52740e880c5752a12f2dc369
Ray <ray@Rays-MacBook-Pro.local>
2022-12-27 01:41:44 +0100

    work

--- test/test.c
+++ test/test.c
@@ -2,21 +2,32 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 int	main(int ac, char **av, char **ev)
 {
 	char	*arg = malloc(sizeof(char) * 10);
+	// int		save =  dup(1);
+	int		fd = open("hal3ar", O_CREAT | O_RDWR, 0777);
+	char	*cat[] = {"cat", "test.c", 0};
 	int	id = 0;
 	int	stat= 0;
 	int	filedes[2];
 	pipe(filedes);
-	dup2(filedes[1], 1);
-	printf("ahahaha\n");
+	dup2(fd, 1);
+	// printf("ahahahasdcsdc\n");
+	// write(1, "ahahaha", 7);
+	// dprintf(1,"ahahaha");
+	// exit(0);	
 	id = fork();
 	if (!id)
 	{
-		read(filedes[0], arg, sizeof(char) * 3);
-		printf("%s\n", arg);
+	// dup2(filedes[0], 0);
+	sleep(1);
+	// read(fd, arg, 10);
+	// fprintf(stderr, "%s\n", arg);
+	exit(0);
 	}
 	waitpid(-1, &stat, 0);
 }
