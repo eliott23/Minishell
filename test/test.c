79b7633d1675eb5a18db206bb1484017f80a6ba0
Abderrahmane Ababachir <aababach@e1r4p9.1337.ma>
2023-01-12 15:37:41 +0100

    w__multiple_pipes

--- test/test.c
+++ test/test.c
@@ -24,24 +24,31 @@ int	main(int ac, char **av, char **ev)
 {
 	char	*arg = malloc(sizeof(char) * 10);
 	int		save =  dup(1);
-	// char	*cat[] = {NULL};
 	char	*grep[] = {"grep", "a", NULL};
 	char	*ls[] = {"ls", 0};
 	char	*cat[] = {"cat", 0};
 	char	*cata[] = {"cat", "a", 0};
-	char	*echo[] = {"echo", "sdav", 0};
+	char	*echo[] = {"echo", "ahahahah", 0};
 	int	id = 0;
 	int	stat = 0;
 	int	i = 0;
 	int	t = 0;
-	//create an array of intigers;
-	int	filedes[8 + 1];
+	int	*filedes = malloc(sizeof(int) * 3);
 	filedes[0] = 0;
-	while (t < 4)
+	while (t < 2)
 	{
-		pipe(filedes + 1 + t*2);
-		t++;
+		pipe(filedes + 1 + t);
+		t += 2;
 	}
+	// id = fork();
+	// if (!id)
+// {	
+	// dup2(filedes[2], 1);
+	// execve("/bin/cat", cat, NULL);
+	// exit(0);
+// }
+// dup2(filedes[1], 0);
+	// execve("/bin/cat", cat, NULL);
 	t = 0;
 	while (i < 4)
 	{
@@ -49,21 +56,29 @@ int	main(int ac, char **av, char **ev)
 	if (!id)
 	{
 		if (i)
-			dup2(filedes[i], 0);
+			dup2(filedes[i - 1], 0);
 		if (i < 2)
-			dup2(filedes[i + 1], 1);
+		{
+			dup2(filedes[i + 2], 1);
+			fprintf(stderr, "went here at i=%d\n", i);
+		}
 		else
+		{
 			dup2(save, 1);
-		// if (i == 0)
-		fprintf(stderr, "%d\n", i);
-			execve("/bin/echo", echo, NULL);
+			fprintf(stderr, "went heere at i=%d\n", i);
+		}
+		if (i == 0)
+			execve("/bin/cat", cat, NULL);
+		// fprintf(stderr, "%d\n", i;
 			// execve("/bin/ls", ls, NULL);
-		// else
-			// execve("/bin/ls", ls, NULL);
+		else
+			execve("/bin/cat", cat, NULL);
+		exit(0);
 	}
 	sleep(1);
 	i += 2;
 	}
+	waitpid(-1, &stat, 0);
 
 	// id = fork();
 	// if (!id)
